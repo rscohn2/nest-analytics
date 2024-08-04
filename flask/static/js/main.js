304c653e698a3eac05e7e112a060bfbd9d90fe87
Robert Cohn <robert.s.cohn@intel.com>
2024-08-04 15:25:20 -0400

    cleanup

--- flask/static/js/main.js
+++ flask/static/js/main.js
@@ -1,29 +1,16 @@
 // This is for dynamically loaded content, we defer until after it is loaded
 function afterLoad() {
-  console.log("After load")
-
   flatpickr(".datetimepicker", {
     enableTime : true,
     dateFormat : "Y-m-d H:i",
   });
 
-  $('.electric-add-form').submit(function(e) {
-    console.log("Electric add form submit triggered")
-    loadPostContent(e, this)
-  });
-  console.log("structures-edit-form length: " +
-              $('.structures-edit-form').length)
-  console.log("structures-edit-button length: " +
-              $('.structures-edit-button').length)
+  $('.electric-add-form').submit(function(e) { loadPostContent(e, this) });
 
-  $('.structures-edit-form').submit(function(e) {
-    console.log("Structures edit form submit triggered")
-    loadPostContent(e, this)
-  });
+  $('.structures-edit-form').submit(function(e) { loadPostContent(e, this) });
 
   $('.structures-edit-button').click(function(e) {
     e.preventDefault();
-    console.log("structure edit button click")
     loadGetContent($(this).data('url'));
   });
 }
@@ -48,7 +35,6 @@ $(document).ready(function() {
   // This is initial page so we can add handlers immediately
   $('.sidebar-link').click(function(e) {
     e.preventDefault();
-    console.log("sidebar click")
     loadGetContent($(this).data('url'));
   });
 });
